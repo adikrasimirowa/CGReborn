1bcb53958e56371d141c24ae029058cf5520751f
adikrasimirowa <adi_krasimirowa@abv.bg>
2016-01-31 21:29:22 +0200

    Update all_doi_view.php

--- application/views/admin/all_doi_view.php
+++ application/views/admin/all_doi_view.php
@@ -1,4 +1,4 @@
-        <div id="page-wrapper">
+              <div id="page-wrapper">
             <div id="page-inner">
                 <div class="row">
                     <div class="col-md-12">
@@ -7,14 +7,32 @@
                         </h1>
                     </div>
                 </div>
+                <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                Изтриване на ДОИ Категория?
+                            </div>
+                            <div class="modal-body">
+                                <img src="<?php echo base_url();?>index.php/assets/img/sign-warning-icon.png" align="left">
+                                <h4> Сигурни ли сте, че искате да изтриете ДОИ Категорията?</h4>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Откажи</button>
+                                <a class="btn btn-danger btn-ok">ИЗТРИЙ</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <!-- /. ROW  -->
                 <div class="row">
                    
-            <div class="col-lg-9">
+            <div class="col-lg-12 ">
                 <div class="panel panel-default">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-lg-12"><a href="add_form" class="btn btn-primary btn-xs pull-right"><b>+</b> Добави нови категории</a><br/>
+                            <div class="col-lg-12"><a href="add_form" class="btn btn-primary btn pull-right"><b>+</b> Добави нови категории</a><br/>
+                                    <div class="table-responsive">
                                     <table class="table table-striped table-bordered table-hover custab">
     <thead>
     
@@ -24,7 +42,8 @@
             <th>Критерия</th>
              <th>Предмет</th>
               <th>Клас</th>
-            <th class="text-center">Настройки</th>
+            <th class="text-center">Редактирай</th>
+            <th class="text-center">Изтрий</th>
         </tr>
     </thead>
           <?php foreach ($all_doi as $key => $value) 
@@ -36,17 +55,14 @@
                         <td> $value[subject_type] </td>
                         <td> $value[class] </td>
                 <td class='text-center'>
-                <a class='btn btn-info btn-xs' href='show_single_doi/$value[doi_id]' ><span class='glyphicon glyphicon-edit'></span> Промени</a> 
-                  ";?> 
-               <button type="button" class="btn btn-danger btn-xs" data-toggle="popover" title="Сигурни ли сте, че искате да премахнете записа?" data-content="<a href='<?php echo " delete/$value[doi_id]"; ?>' title='изтриване'> <h4>Да </h4></a>"><span class='glyphicon glyphicon-edit'></span>Изтрий</button>
-
-             <?php  echo"  </td>
-      
+                <a class='btn btn-info' href='show_single_doi/$value[doi_id]' ><span class='glyphicon glyphicon-edit'></span> Редактирай</a> 
+               </td> <td>  <button class='btn btn-danger' data-href='".base_url()."index.php/doi/delete/".$value['doi_id']."' data-toggle='modal' data-target='#confirm-delete'>Изтрий</button></td></td>      
             </tr> "; 
         }
             ?>
             
     </table>
+                        </div>
                         </div>
                     </div>
                 </div>
@@ -57,7 +73,10 @@
 <!-- /. PAGE INNER  -->
 </div>
 <!-- /. PAGE WRAPPER  -->
-<script type="text/javascript"> 
-$(function () {
-  $('[data-toggle="popover"]').popover({html:true})
-})</script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#confirm-delete').on('show.bs.modal', function(e) {
+            $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+        });
+    });
+</script>
