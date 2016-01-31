e0911510945f4567070ec7e1c90849584bbd79df
adikrasimirowa <adi_krasimirowa@abv.bg>
2016-01-31 23:37:28 +0200

    Update add_doi_view.php

--- application/views/admin/add_doi_view.php
+++ application/views/admin/add_doi_view.php
@@ -16,19 +16,19 @@
       <?php echo form_open('doi/add_new_doi'); 
       echo validation_errors('<div class="alert alert-danger" role="alert">', '</div>');
       ?>
-      <label>Профил :</label><?php echo form_input(array( 'class'=>'form-control','name' => 'profile')); ?>
-
+      <label>Профил :</label><?php echo form_input(array( 'class'=>'form-control','name' => 'profile_short')); ?>
+      <label>Описание :</label><?php echo form_input(array( 'class'=>'form-control','name' => 'profile')); ?>
       <label>Критерия : </label><?php echo form_input(array( 'class'=>'form-control','name' => 'criteria')); ?>
-     
+   
       <?php echo "
       <label>Клас : </label>
       <select name='class' class='form-control'>";
  echo "<option value=''></option>";
 $i = 0;
-while($i <= count($n_class)){
+while($i < count($n_class)){
   $val= $n_class[$i]['value'];
   $des = $n_class[$i]['n_class'];
-  echo "<option value='$i'>$des</option>";
+  echo "<option value='$i'> $des</option>";
   $i++;
 }
 echo "</select>";
@@ -37,7 +37,7 @@ echo "<label>Предмет : </label>
 <select name='subject' class='form-control'>";
  echo "<option value=''></option>";
 $i = 0;
-while($i <= count($n_subject)){
+while($i < count($n_subject)){
   $val= $n_subject[$i]['value'];
   $des = $n_subject[$i]['subject_type'];
   echo "<option value='$i'>$des</option>";
